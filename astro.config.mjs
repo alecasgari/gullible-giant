11ff9b78b34fe5bde35e1200c313e62870de02e8
Alec Asgari <asgarialec@gmail.com>
2025-09-28 16:39:49 +0400

    feat(alias): add tina-client alias and update imports; fix admin asset; add Vite alias

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import { fileURLToPath } from 'node:url';
 
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react()],
+  vite: {
+    resolve: {
+      alias: {
+        'tina-client': fileURLToPath(new URL('./tina/__generated__/client.ts', import.meta.url)),
+        '~': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
+  },
 });
