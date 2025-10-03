db09d07cbc405eca7b2acd46c0cccc19007bdbe5
alecadmin <alecadmin@vmi2779817>
2025-10-03 12:05:21 +0400

    latest chanegs after sytem breakup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,12 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-import { fileURLToPath } from 'node:url';
 
 import react from '@astrojs/react';
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
   vite: {
-    resolve: {
-      alias: {
-        'tina-client': fileURLToPath(new URL('./tina/__generated__/client.ts', import.meta.url)),
-        '~': fileURLToPath(new URL('./src', import.meta.url)),
-      },
-    },
+    server:  { host: true, allowedHosts: true },
+    preview: { host: true, allowedHosts: true }
   },
+
+  integrations: [react()]
 });
