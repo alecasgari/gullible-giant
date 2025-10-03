644c21287e32d1010860bc5c0b4f2af13574479a
Alec Asgari <asgarialec@gmail.com>
2025-10-03 13:33:59 +0400

    seo: enable sitemap, canonical, robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 import react from '@astrojs/react';
 
 export default defineConfig({
+  site: 'https://alecasgari.com',
   vite: {
     server:  { host: true, allowedHosts: true },
     preview: { host: true, allowedHosts: true }
   },
 
-  integrations: [react()]
+  integrations: [react(), sitemap()]
 });
